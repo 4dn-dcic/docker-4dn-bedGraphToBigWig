0d9acb181c9e90afc6ea11863657cc20bd44602a
Lmercadom <42242797+Lmercadom@users.noreply.github.com>
2018-11-16 11:34:21 -0500

    Update run-bedtobigwig.sh

--- scripts/run-bedtobigwig.sh
+++ scripts/run-bedtobigwig.sh
@@ -1,22 +1,30 @@
 #!/bin/bash
 
-# Runs bedtobigwig. Converts a bedgraph file into a bigwig file.
+# Aggregates a Bed file to a format compatible with higlass (beddb)
 
 INPUT=$1
-CHROMESIZE=$2
+ASSEMBLY=$2
 OUTDIR=$3
 
 FILE_BASE=$(basename $INPUT)
-FILE_NAME=${FILE_BASE%%.*}
+FILE_NAME=${FILE_BASE%.*}
+echo $FILE_NAME
 
 mkfifo pp
 
 if [ ! -d "$OUTDIR" ]
-then 
- mkdir $OUTDIR
+then
+  mkdir $OUTDIR
 fi
 
-gunzip -c $INPUT > pp.bedGraph
-bedGraphToBigWig pp.bedGraph $CHROMESIZE $OUTDIR/$FILE_NAME.bw
+gunzip -c $INPUT > pp.bed
+
+outputfile="$OUTDIR/$FILE_NAME.beddb"
+echo $outputfile
+
+clodius aggregate bedfile \
+  --assembly $ASSEMBLY -o $outputfile \
+   pp.bed
+
 rm pp
-rm pp.bedGraph
+rm pp.bed
